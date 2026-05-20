f46ab6621aeafa57a0fcdd764987ca0670f9f3a9
Luka Saarivirta <luka.saarivirta@gmail.com>
2026-05-20 19:24:52 +0300

    added directory traverser

--- src/findit/crawler/crawler.rs
+++ src/findit/crawler/crawler.rs
@@ -45,3 +45,31 @@ pub fn traverse_files(start_path: &str) -> Vec<String> {
 
     files
 }
+
+pub fn traverse_dirs(start_path: &str) -> Vec<String> {
+    let mut dirs: Vec<String> = Vec::new();
+
+    if let Ok(entries) = std::fs::read_dir(start_path) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            let path_str = path.to_string_lossy();
+
+            if should_ignore(&path_str) {
+                let msg = format!("Ignoring '{}'", path_str);
+                logger::info(&msg);
+                continue;
+            }
+
+            if path.is_dir() {
+                let msg = format!("Dir '{}', adding", path_str);
+                logger::info(&msg);
+
+                dirs.push(path_str.clone().into_owned());
+
+                dirs.extend(traverse_dirs(path_str.as_ref()));
+            }
+        }
+    }
+
+    dirs
+}
